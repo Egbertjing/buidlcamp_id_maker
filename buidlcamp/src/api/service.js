f366559eb798ee7e763fa6a3442465dfc648b96b
JING Huihao <91329273+Egbertjing@users.noreply.github.com>
2022-10-11 16:48:16 +0800

    fix: fix the bug

--- buidlcamp/src/api/service.js
+++ buidlcamp/src/api/service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const service = axios.create({
   // baseURL: 'http://127.0.0.1:6010',
-  baseURL: 'https://furion.io:3609',
+  baseURL: 'http://furion.io:3609',
   timeout: 6000000,
 });
 
