5977617a6dc59231c5a2b752b432117d62e7dd1e
JING Huihao <91329273+Egbertjing@users.noreply.github.com>
2022-10-11 13:41:26 +0800

    build: https

--- buidlcamp/vite.config.ts
+++ buidlcamp/vite.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from "node:path";
+import fs from "node:fs";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3610,
+    host: 'buidlcamp.web3edu.xyz',
+    // https: {
+    //   key: fs.readFileSync('./__web3edu_xyz.p7b'),
+    //   cert: fs.readFileSync('./__web3edu_xyz.crt')
+    // },
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
